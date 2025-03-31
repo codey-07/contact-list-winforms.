7901862f6720a532e50958d12b37f2601b06e212
TienQuo <tanobi.va@gmail.com>
2025-03-31 21:14:04 +0700

    Thêm Form để thêm liên hệ mới

---
+++ ContactList/ContactList/AddContactForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactList
+{
+    public partial class AddContactForm : Form
+    {
+        public AddContactForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string fullName = txtFullName.Text;
+            string phoneNumber = txtPhoneNumber.Text;
+
+            MessageBox.Show($"Đã thêm liên hệ:\nHọ tên: {fullName}\nSố điện thoại: {phoneNumber}",
+                            "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
--- ContactList/ContactList/MainForm.Designer.cs
+++ ContactList/ContactList/MainForm.Designer.cs
@@ -33,19 +33,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(220, 33);
+            this.button1.Location = new System.Drawing.Point(165, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.Size = new System.Drawing.Size(97, 19);
             this.button1.TabIndex = 0;
             this.button1.Text = "Thêm liên hệ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // btnAddContact
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "btnAddContact";
             this.Text = "Form1";
             this.ResumeLayout(false);
