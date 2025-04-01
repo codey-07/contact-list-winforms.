dadd54c27f5edb6af2a363a6c91f7832c5f43819
TienQuo <Tanobi.va@gmail.com>
2025-04-01 19:40:59 +0700

    Sửa đổi AddContactForm

--- ContactList/ContactList/AddContactForm.Designer.cs
+++ ContactList/ContactList/AddContactForm.Designer.cs
@@ -32,12 +32,13 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.txtFullName = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblThongTinBoSung = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtPhoneNumber
             // 
             this.txtPhoneNumber.Location = new System.Drawing.Point(278, 181);
-            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
             this.txtPhoneNumber.Size = new System.Drawing.Size(132, 22);
             this.txtPhoneNumber.TabIndex = 0;
@@ -46,7 +47,7 @@
             // btnSave
             // 
             this.btnSave.Location = new System.Drawing.Point(422, 286);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(100, 28);
             this.btnSave.TabIndex = 1;
@@ -57,7 +58,7 @@
             // txtFullName
             // 
             this.txtFullName.Location = new System.Drawing.Point(490, 53);
-            this.txtFullName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFullName.Margin = new System.Windows.Forms.Padding(4);
             this.txtFullName.Name = "txtFullName";
             this.txtFullName.Size = new System.Drawing.Size(132, 22);
             this.txtFullName.TabIndex = 0;
@@ -71,16 +72,26 @@
             this.textBox1.TabIndex = 2;
             this.textBox1.Text = "Email";
             // 
+            // lblThongTinBoSung
+            // 
+            this.lblThongTinBoSung.AutoSize = true;
+            this.lblThongTinBoSung.Location = new System.Drawing.Point(422, 413);
+            this.lblThongTinBoSung.Name = "lblThongTinBoSung";
+            this.lblThongTinBoSung.Size = new System.Drawing.Size(113, 16);
+            this.lblThongTinBoSung.TabIndex = 3;
+            this.lblThongTinBoSung.Text = "Thông tin bổ sung";
+            // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.lblThongTinBoSung);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtFullName);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtPhoneNumber);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "AddContactForm";
             this.Text = "AddContactForm";
             this.ResumeLayout(false);
@@ -94,5 +105,6 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.TextBox txtFullName;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblThongTinBoSung;
     }
 }
